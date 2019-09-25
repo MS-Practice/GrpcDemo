a6d37819ed325e8b5b143f0fad6ae8a30f1bc43d
marsonshine <ms27946@outlook.com>
2019-09-25 12:39:02 +0800

    init:init project

---
+++ GrpcGreeterClient/Program.cs
@@ -0,0 +1,24 @@
+﻿using Grpc.Net.Client;
+using GrpcDemo;
+using System;
+using System.Threading.Tasks;
+
+namespace GrpcGreeterClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Greeter.GreeterClient(channel);
+            // 方式1
+            var reply = await client.SayHelloAsync(
+                new HelloRequest { Name = "GreeterClient" }
+                );
+            // 方式2：流服务器调用
+            Console.WriteLine("Greeting: " + reply.Message);
+            Console.WriteLine("请按任意键结束...");
+            Console.ReadKey();
+        }
+    }
+}
