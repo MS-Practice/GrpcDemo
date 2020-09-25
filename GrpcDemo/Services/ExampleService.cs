1d96accd59017cd58eba91a3eeecf5ac4fb14f84
maoshuai <maoshuai@kingsunsoft.com>
2020-09-25 15:36:11 +0800

    fix: grpc.net 基本概念，类型，decimal，客户端调用基本操作

--- GrpcDemo/Services/ExampleService.cs
+++ GrpcDemo/Services/ExampleService.cs
@@ -1,11 +1,41 @@
+using System;
 using System.Threading.Tasks;
 using Grpc.Core;
 
 namespace GrpcDemo.Services {
     public class ExampleService : GrpcDemo.ExampleService.ExampleServiceBase {
-        public override Task<ExampleResponse> UnaryCall(ExampleRequest request, ServerCallContext context) {
-            var response = new ExampleResponse();
-            return Task.FromResult(response);
+        public override Task<ExampleResponse> UnaryCall (ExampleRequest request, ServerCallContext context) {
+            var response = new ExampleResponse ();
+            return Task.FromResult (response);
+        }
+
+        public override async Task StreamingFromServer (ExampleRequest request, IServerStreamWriter<ExampleResponse> responseStream, ServerCallContext context) {
+            for (var i = 0; i < 5; i++) {
+                // È¡Ïû²Ù×÷
+                while (!context.CancellationToken.IsCancellationRequested)
+                {
+                    await responseStream.WriteAsync(new ExampleResponse());
+                    await Task.Delay(TimeSpan.FromSeconds(1));
+                }
+            }
+        }
+
+        public override async Task<ExampleResponse> StreamingFromClient(IAsyncStreamReader<ExampleRequest> requestStream, ServerCallContext context)
+        {
+            await foreach (var message in requestStream.ReadAllAsync())
+            {
+
+            }
+            return new ExampleResponse();
+        }
+
+        public override async Task StreamingBothWays(IAsyncStreamReader<ExampleRequest> requestStream,
+    IServerStreamWriter<ExampleResponse> responseStream, ServerCallContext context)
+        {
+            await foreach (var message in requestStream.ReadAllAsync())
+            {
+                await responseStream.WriteAsync(new ExampleResponse());
+            }
         }
     }
 }
--- GrpcGreeterClient/Program.cs
+++ GrpcGreeterClient/Program.cs
@@ -1,10 +1,17 @@
-﻿using Grpc.Net.Client;
+﻿using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using Grpc.Net.Client;
 using GrpcDemo;
 using System;
 using System.Threading.Tasks;
 
 namespace GrpcGreeterClient
 {
+    /// <summary>
+    /// 创建Channel是很昂贵的操作,一旦创建,它是长连接,所以可以重用.应该将它缓存起来(单例)
+    /// 由Channel创建的客户端Client,这是一个轻量的对象,不需要缓存和重新.
+    /// 单个channel可以创建不同类型的client
+    /// </summary>
     class Program
     {
         static async Task Main(string[] args)
@@ -15,7 +22,34 @@ namespace GrpcGreeterClient
             var reply = await client.SayHelloAsync(
                 new HelloRequest { Name = "GreeterClient" }
                 );
-            // 方式2：流服务器调用
+
+
+            //var payload = new PayloadResponse();
+            var status = new GrpcDemo.Status();
+            status.Detail = Any.Pack(new HelloRequest() { Name = "marsonshine" });
+            if (status.Detail.Is(HelloRequest.Descriptor))
+            {
+
+            }
+
+            // oneof
+            try
+            {
+                var oneofResult = await client.OneofReturnValueAsync(new Empty(), deadline: DateTime.UtcNow.AddSeconds(5));  // 还可以设置deadline,更有利于系统的伸缩性
+                switch (oneofResult.ResultCase)
+                {
+                    case ResponseMessage.ResultOneofCase.Person:
+                        break;
+                    case ResponseMessage.ResultOneofCase.Error:
+                        break;
+                    default:
+                        break;
+                }
+            }
+            catch (RpcException ex) when (ex.StatusCode == StatusCode.DeadlineExceeded)
+            {
+                Console.WriteLine("Timeout!");
+            }
             Console.WriteLine("Greeting: " + reply.Message);
             Console.WriteLine("请按任意键结束...");
             Console.ReadKey();
